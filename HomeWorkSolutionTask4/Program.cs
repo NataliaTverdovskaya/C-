323996f1ea7a5002df354770f9a6fdd6dda13f20
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 01:08:57 +0700

    add HomeWorkSolutionTask4

---
+++ HomeWorkSolutionTask4/Program.cs
@@ -0,0 +1,38 @@
+﻿string inputNumberA = Console.ReadLine();
+string inputNumberB = Console.ReadLine();
+string inputNumberC = Console.ReadLine();
+
+if(inputNumberA != null & inputNumberB != null & inputNumberC != null)
+{
+int NumberA = int.Parse(inputNumberA);
+int NumberB = int.Parse(inputNumberB);
+int NumberC = int.Parse(inputNumberC);
+
+    if (NumberA > NumberB)
+    {
+        if (NumberA > NumberC)
+        {
+            Console.Write("max=");
+            Console.WriteLine(NumberA); 
+        }
+        else
+        {
+            Console.Write("max=");
+            Console.WriteLine(NumberC); 
+        }
+    }
+    else
+    {
+        if (NumberB > NumberC)
+        {
+            Console.Write("max=");
+            Console.WriteLine(NumberB); 
+        }
+        else
+        {
+            Console.Write("max=");
+            Console.WriteLine(NumberC); 
+        }
+    }
+}    
+
