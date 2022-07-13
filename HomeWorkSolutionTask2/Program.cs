7ce5e5550036ea4f2834062025b9f24eabcf061c
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 00:37:02 +0700

    add HomeWorkSolutionTask2

---
+++ HomeWorkSolutionTask2/Program.cs
@@ -0,0 +1,13 @@
+﻿string inputNumberA = Console.ReadLine();
+string inputNumberB = Console.ReadLine();
+
+int NumberA = int.Parse(inputNumberA);
+int NumberB = int.Parse(inputNumberB);
+
+    if(NumberA > NumberB)
+    Console.WriteLine("max=", NumberA);
+    else
+    {
+    Console.Write("max=");
+    Console.WriteLine(NumberB);
+    }
