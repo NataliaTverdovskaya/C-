00c91f645ecd33544f0651f01958197cc41d47f0
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-13 12:04:36 +0700

    dd solutionTask0

---
+++ SolutionTask0/Program.cs
@@ -0,0 +1,9 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+
+    int outNumber = (int)Math.Pow(inputNumber, 2);
+    Console.WriteLine(outNumber);
+}
