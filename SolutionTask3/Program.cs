34931eb3947ff28a6431541204dc43adac3ec9aa
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-13 20:41:22 +0700

    add SolutionTask3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,14 @@
+﻿string? inputLine = Console.ReadLine();
+
+int N = int.Parse(inputLine);
+int S = N*-1;
+
+string Line = " ";
+
+while (S < N)
+{
+    Line = Line + S + ", ";
+    S = S + 1;
+}
+Line = Line + N;
+Console.WriteLine(Line);
