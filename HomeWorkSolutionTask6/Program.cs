c25fbb2184861e4996be7a585b1aa522ca9fa69f
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 01:17:27 +0700

    add HomeWorkSolutionTask6

---
+++ HomeWorkSolutionTask6/Program.cs
@@ -0,0 +1,16 @@
+﻿string inputNumberA = Console.ReadLine();
+
+if(inputNumberA != null)
+{
+int numberA = int.Parse(inputNumberA);
+int numberB = numberA%2;
+
+if (numberB == 0)
+{
+    Console.WriteLine("четное");
+}
+else
+{
+    Console.WriteLine("нечетное");
+}
+}
