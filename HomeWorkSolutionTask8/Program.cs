925e379c020256cd9f450bb201a6349cf9a07144
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 01:29:51 +0700

    add HomeWorkSolutionTask8

---
+++ HomeWorkSolutionTask8/Program.cs
@@ -0,0 +1,19 @@
+﻿string inputNumberN = Console.ReadLine();
+
+if(inputNumberN != null)
+{
+int numberN = int.Parse(inputNumberN);
+int startNumber = 1;
+string line = "";
+
+while (startNumber <= numberN)
+{
+    int numberB = startNumber%2;
+    if (numberB == 0)
+    {
+        line = line + " " + startNumber;
+    }
+    startNumber++;
+}
+Console.WriteLine(line);
+}
