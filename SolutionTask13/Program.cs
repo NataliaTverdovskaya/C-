020c246674d00a98aba869d1c1abb399e5f281b1
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 20:13:54 +0700

    add SolutionTask13

---
+++ SolutionTask13/Program.cs
@@ -0,0 +1,27 @@
+﻿string? inputNumber1 = Console.ReadLine();
+string? inputNumber2 = Console.ReadLine();
+
+
+if (inputNumber1 != null & inputNumber2 != null)
+{
+    int Number1 = int.Parse(inputNumber1);
+    int Number2 = int.Parse(inputNumber2);
+    int result1 = Number2*Number2;
+    int result2 = Number1*Number1;
+
+    if(Number1 == result1)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        if(Number2 == result2)
+        {
+             Console.WriteLine("да");
+        }
+        else
+        {
+            Console.WriteLine("нет");
+        }
+    }
+}
