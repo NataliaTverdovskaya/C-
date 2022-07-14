7b50cc98a0d888b1bf328cc863642b3eaf6c5f62
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 19:57:53 +0700

    add SolutionTask12

--- SolutionTask10/Program.cs
+++ SolutionTask10/Program.cs
@@ -6,11 +6,11 @@ char[] charArray = randomNumber.ToString().ToCharArray();
 
 string charFirst = charArray[0].ToString();
 string charThird = charArray[2].ToString();
-Console.WriteLine(charFirst+charThird);
+Console.WriteLine(charFirst + charThird);
 
 //Variant 2
-int number1 = randomNumber/100;
-int number2 = randomNumber%10;
-int finalNumber = number1*10+number2;
+int number1 = randomNumber / 100;
+int number2 = randomNumber % 10;
+int finalNumber = number1 * 10 + number2;
 
 Console.WriteLine(finalNumber);
---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,17 @@
+﻿string? inputNumber = Console.ReadLine();
+
+if (inputNumber != null)
+{
+    int number = int.Parse(inputNumber);
+    int result7 = number%7;
+    int result23 = number%23;
+
+    if(result7 == 0 & result23 == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+      Console.WriteLine("нет");  
+    }
+}
