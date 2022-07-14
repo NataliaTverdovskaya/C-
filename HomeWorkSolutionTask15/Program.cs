ff8c8c1255a91dd497194ff77fc1d94f71cf2704
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 23:33:54 +0700

    add HomeWorkSolutionTask15

---
+++ HomeWorkSolutionTask15/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+
+string? inputNumber = Console.ReadLine();
+
+if (inputNumber != null)
+{
+    int number = int.Parse(inputNumber);
+    if ((number < 8) & (number != 0))
+    {
+        if ((number == 6) || (number == 7))
+        {
+            Console.WriteLine("Это выходной");
+        }
+        else
+        {
+            Console.WriteLine("Это не выходной");
+        }
+    }
+    else
+    {
+       Console.WriteLine("Некорректное число"); 
+    }
+}
