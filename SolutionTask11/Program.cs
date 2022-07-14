48614dd7dcf13b053a8550a8700aca1f4545278f
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 19:45:46 +0700

    add SolutionTask11

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,18 @@
+﻿string? inputNumber1 = Console.ReadLine();
+string? inputNumber2 = Console.ReadLine();
+
+
+if (inputNumber1 != null & inputNumber2 != null)
+{
+    int Number1 = int.Parse(inputNumber1);
+    int Number2 = int.Parse(inputNumber2);
+    int result = Number2 % Number1;
+    if (result == 0)
+    {
+        Console.WriteLine("кратно");
+    }
+    else
+    {
+        Console.WriteLine("некратно, остаток " + result);
+    }
+}
