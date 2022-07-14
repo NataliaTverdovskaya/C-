49b4cba1231afbf2645a4a0959fd902d9e20a463
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 23:19:22 +0700

    add HomeWorkSolutionTask13

---
+++ HomeWorkSolutionTask13/Program.cs
@@ -0,0 +1,14 @@
+﻿string? inputNumber = Console.ReadLine();
+
+if (inputNumber != null)
+{
+    char[] number = inputNumber.ToCharArray();
+    if (number.Length >= 3)
+    {
+        Console.WriteLine(number[2]);
+    }
+    else
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+}
