74c9a54f99db3c053d6bf2df7332fcc67a474a07
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 14:45:58 +0700

    add ST10

---
+++ SolutionTask10/Program.cs
@@ -0,0 +1,16 @@
+﻿int randomNumber = new Random().Next(100, 999);
+Console.WriteLine(randomNumber);
+
+//Variant 1
+char[] charArray = randomNumber.ToString().ToCharArray();
+
+string charFirst = charArray[0].ToString();
+string charThird = charArray[2].ToString();
+Console.WriteLine(charFirst+charThird);
+
+//Variant 2
+int number1 = randomNumber/100;
+int number2 = randomNumber%10;
+int finalNumber = number1*10+number2;
+
+Console.WriteLine(finalNumber);
