e0203e63c46cab535b16ce2581a091e182a08029
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 23:12:32 +0700

    add HomeWorkSolutionTask10

---
+++ HomeWorkSolutionTask10/Program.cs
@@ -0,0 +1,8 @@
+﻿string? inputNumber = Console.ReadLine();
+
+if (inputNumber != null)
+{
+    int number = int.Parse(inputNumber);
+    int result = (number%100)/10;
+    Console.WriteLine(result);
+}
