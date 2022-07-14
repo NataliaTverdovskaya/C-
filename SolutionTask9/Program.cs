71f5ff3b8eb1dbc47ec470d2f2849abe17487fca
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-14 14:25:32 +0700

    add ST9

---
+++ SolutionTask9/Program.cs
@@ -0,0 +1,22 @@
+﻿System.Random numberSintezator = new Random();
+
+
+void variant1 (System.Random numberSintezator)
+{
+int number = numberSintezator.Next(10, 100);
+ Console.WriteLine(number);
+
+int firstNumber = number/10;
+int secondNumber = number%10;
+
+if(firstNumber > secondNumber)
+{
+    Console.WriteLine(firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber);
+}
+}
+
+variant1 (numberSintezator);
