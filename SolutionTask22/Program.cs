e6b11c94590df9d7520fd0bc4b27b6b7478b67b5
NatalyaTverdovskaya <michelle911@mail.ru>
2022-07-17 19:57:50 +0700

    add SolutionTask22

---
+++ SolutionTask22/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите число N: ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int N = int.Parse(inputLine);
+    int S = 1;
+    double pow = 1;
+    string lineN = "";
+    string lineNN = "";
+
+
+    while (S <= N)
+    {
+        pow = Math.Pow(S, 2);
+        lineN = lineN + S + ", ";
+        lineNN = lineNN + pow + ", ";
+        S++;
+    }
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNN);
+}
