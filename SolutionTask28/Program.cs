fa37bb3c82e9226cff6fc0369047bcea56cb3b77
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-07 10:38:56 +0700

    add solutiontask 28

---
+++ SolutionTask28/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int inputNumber = int.Parse(Console.ReadLine());
+int t;
+void VariantSimple()
+{
+    long prOfNumber = 1;
+    for (long i = 1; i<= inputNumber; i++)
+    {
+        prOfNumber=prOfNumber*i;
+    }
+    Console.WriteLine("Произведение чисел от 1 до "+inputNumber+" равнo "+prOfNumber);
+}
+
+
+
+t = Environment.TickCount;
+VariantSimple();
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount - t);
