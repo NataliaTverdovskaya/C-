b3e6528a304fe3d5bef0b432b5eb7f80fd543d69
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-08 21:43:32 +0700

    add HomeWorkSolutiontask 29

---
+++ HomeWorkSolutionTask29/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Random numberSintezator = new Random();
+
+void VariantNaive()
+{
+    int i = 0;
+    
+    int[] array = new int[7];
+
+    Console.Write("[");
+
+    while (i < 7)
+    {
+        array[i] = numberSintezator.Next(0, 99);
+        Console.Write(array[i] + ",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(0, 99));
+    Console.Write("]");
+}
+VariantNaive();
