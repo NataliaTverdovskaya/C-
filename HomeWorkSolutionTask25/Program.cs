e283675c2bf8c87f46321c4843480ee1bcc8ab72
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-08 13:19:02 +0700

    add HomeWorkSolutiontask 25

---
+++ HomeWorkSolutionTask25/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+double inputNumber1 = double.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+double inputNumber2 = double.Parse(Console.ReadLine());
+
+double VariantMath()
+{
+    double result = 0;
+    result = Math.Pow(inputNumber1, inputNumber2);
+    return result;
+}
+
+double VariantWhile()
+{
+    int i = 0;
+    double result = 1;
+    while (i<inputNumber2)
+    {
+    result = result*inputNumber1;
+    i++;
+    }
+    return result;
+}
+
+Console.WriteLine(VariantMath());
+Console.WriteLine(VariantWhile());
+
