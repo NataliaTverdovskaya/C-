22b9322a86466de4fbf5c0e5aba885407c816f76
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-08 13:28:53 +0700

    add Solutiontask 30

---
+++ SolutionTask30/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Random numberSintezator = new Random();
+
+void VariantNaive()
+{
+    int i = 0;
+    Console.Write("[");
+
+    while (i < 8 - 1)
+    {
+        Console.Write(numberSintezator.Next(0, 2) + ",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(0, 2));
+    Console.Write("]");
+}
+VariantNaive();
