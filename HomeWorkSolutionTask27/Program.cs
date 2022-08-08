c34918f1dfceee54ee1365bedd367c8344d83542
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-08 13:40:43 +0700

    add HomeWorkSolutiontask 27

---
+++ HomeWorkSolutionTask27/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int inputNumber = int.Parse(Console.ReadLine());
+
+int t;
+
+int Sum()
+{
+int sum = 0;
+while (inputNumber>0)
+{
+    sum = sum + inputNumber%10;
+    inputNumber=inputNumber/10;
+}
+    return sum;
+}
+
+t = Environment.TickCount;
+Console.WriteLine("Сумма цифр: "+Sum());
+Console.WriteLine("Sum time: {0} ms", Environment.TickCount - t);
