848c802c97964e34e2241eea54d3088cff648180
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-09 20:18:00 +0700

    add HomeWorkSolutiontask 29+*

--- HomeWorkSolutionTask29/Program.cs
+++ HomeWorkSolutionTask29/Program.cs
@@ -17,4 +17,35 @@ void VariantNaive()
     Console.Write(numberSintezator.Next(0, 99));
     Console.Write("]");
 }
+
+void Variant2 (int arrayLength, int min, int max)
+{
+    int i = 0;
+    
+    int[] array = new int[arrayLength];
+
+    Console.Write("[");
+
+    while (i < arrayLength-1)
+    {
+        array[i] = numberSintezator.Next(min, max);
+        Console.Write(array[i] + ",");
+        i++;
+    }
+    Console.Write(numberSintezator.Next(min, max));
+    Console.Write("]");
+}
+
 VariantNaive();
+
+Console.WriteLine(" ");
+Console.Write ("Введите длину массива: ");
+int arrayLength = int.Parse(Console.ReadLine());
+
+Console.Write ("Введите диапазон значений элементов от: ");
+int min = int.Parse(Console.ReadLine());
+
+Console.Write ("до: ");
+int max = int.Parse(Console.ReadLine());
+
+Variant2 (arrayLength, min, max);
