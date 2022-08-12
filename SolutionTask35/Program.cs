7393519b87f199c5e704103071a2dd671a9d93cd
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-12 11:35:07 +0700

    add SolutionTask35

---
+++ SolutionTask35/Program.cs
@@ -0,0 +1,41 @@
+﻿//заполнение массива
+int[] ArrayCreation()
+{
+    int i = 0;
+    int[] array = new int[123];
+    System.Random numberSintezator = new Random();
+    while (i < 123)
+    {
+        array[i] = numberSintezator.Next();
+        i++;
+    }
+    return array;
+}
+
+//выводит массив
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    Console.Write(string.Join(",", array));
+    Console.WriteLine("]");
+}
+
+//решение задачи 35
+int CalculateTask(int[] array, int downBoarder, int upBoarder)
+{
+    int count = 0;
+    int i = 0;
+    while (i < array.Length)
+    {
+        if (array[i] > downBoarder && array[i] < upBoarder)
+        {
+            count++;
+        }
+        i++;
+    }
+    return count;
+}
+
+int[] bufArray = ArrayCreation();
+PrintArray(bufArray);
+Console.WriteLine("Количество элементов массива: " + CalculateTask(bufArray,10,99));
