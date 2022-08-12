a5e352a90bf32b9403549ef707d7d8388e76f1e6
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-12 21:13:21 +0700

    add SolutionTask42

---
+++ SolutionTask42/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+Console.WriteLine(Convert.ToString(int.Parse(Console.ReadLine()),2));
+
+int number = int.Parse(Console.ReadLine());
+string outLine = Convert.ToString(number,2);
+Console.WriteLine(outLine);
