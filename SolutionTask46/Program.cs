10222388a9eb22d8cb9a6cc2ecd8a9aa0b2f0a3d
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-16 11:51:38 +0700

    add SolutionTask46

---
+++ SolutionTask46/Program.cs
@@ -0,0 +1,50 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными целыми числами
+
+ConsoleColor[] col = new ConsoleColor[] {ConsoleColor.Black, ConsoleColor.Blue, ConsoleColor.Magenta, ConsoleColor.Yellow};
+
+//заполнение двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    int[,] array = new int[countRow, countColumn];
+    System.Random numberSintezator = new Random();
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array[i, j] = numberSintezator.Next(0, 101);
+            //Console.WriteLine("");
+        }
+    }
+    return array;
+}
+
+//выводит двухмерный массив
+void PrintTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintColorTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0,4)];
+            Console.Write(array[i,j] + " ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] testArray = FillTwoDimArray(10, 10);
+PrintTwoDimArray(testArray);
+PrintColorTwoDimArray(testArray);
