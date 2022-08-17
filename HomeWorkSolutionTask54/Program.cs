833929edfde2bfbaf1a467939f52e30367f8f606
NatalyaTverdovskaya <michelle911@mail.ru>
2022-08-17 21:36:27 +0700

    add 54

---
+++ HomeWorkSolutionTask54/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. 
+//Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+//заполнение двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    int[,] array = new int[countRow, countColumn];
+    System.Random numberSintezator = new Random();
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array[i, j] = numberSintezator.Next(0, 100);
+            //Console.WriteLine("");
+        }
+    }
+    return array;
+}
+
+//выводит двухмерный массив
+void PrintTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+//метод упорядочивает строки по убыванию
+int[,] SortRowsDown(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            for (int k = 0; k < inputArray.GetLength(1) - 1; k++)
+            {
+                if (inputArray[i, k] < inputArray[i, k + 1])
+                {
+                    int temp = inputArray[i, k + 1];
+                    inputArray[i, k + 1] = inputArray[i, k];
+                    inputArray[i, k] = temp;
+                }
+            }
+        }
+    }
+    return inputArray;
+}
+
+int[,] twoDimArray = FillTwoDimArray(5,5);
+PrintTwoDimArray(twoDimArray);
+Console.WriteLine("");
+
+int[,] sortTwoDimArray=SortRowsDown(twoDimArray);
+PrintTwoDimArray(sortTwoDimArray);
